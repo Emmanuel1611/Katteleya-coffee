ccac0995ecb10921a32228fcef3a9b51f5ea29ed
Emmanuel1611 <muhindoemma39@gmail.com>
2026-03-29 21:54:32 +0300

    update shop.html and Views

--- cattleya_coffee/shop/views.py
+++ cattleya_coffee/shop/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db.models import Q
+from decimal import Decimal
 from core.models import Product
 
 # Create your views here.
@@ -6,8 +8,26 @@ from core.models import Product
 from django.shortcuts import render
 
 def shop_page(request):
-    products = Product.objects.filter(is_available=True).order_by('-created')[:6]
-    return render(request, 'shop.html', {'products': products})
+    query = request.GET.get('q', '').strip()
+    min_price_str = request.GET.get('min_price')
+    max_price_str = request.GET.get('max_price')
+    products = Product.objects.filter(is_available=True)
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    if min_price_str:
+        products = products.filter(price__gte=Decimal(min_price_str))
+    if max_price_str:
+        products = products.filter(price__lte=Decimal(max_price_str))
+    products = products.order_by('-created')
+    context = {
+        'products': products, 
+        'query': query,
+        'min_price': min_price_str or '',
+        'max_price': max_price_str or ''
+    }
+    if request.GET.get('partial') == 'true':
+        return render(request, 'shop/_products_partial.html', context)
+    return render(request, 'shop.html', context)
 
 def cart_page(request):
     return render(request, 'shop/cart.html')
